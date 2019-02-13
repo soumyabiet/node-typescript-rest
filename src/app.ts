45a463818137641f404e32f7b80192aa83965b5a
Soumya Biswas <soumyabiet@gmail.com>
2019-02-13 16:39:23 +0530

    Add editor config and linting

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
-import * as express from 'express';
 import { json, urlencoded } from 'body-parser';
-
-
+import * as express from 'express';
 import { Routes } from './routes/routes';
-
 
 class App {
     private app: express.Application;
-    private route: Routes =  new Routes();
+    private route: Routes = new Routes();
 
     constructor() {
         this.config();
@@ -19,12 +16,12 @@ class App {
 
     private config(): void {
         this.app = express();
-        this.app.use( json() );
-        this.app.use( urlencoded({ extended: false }));
-        this.app.use( express.static('public') );
+        this.app.use(json());
+        this.app.use(urlencoded({ extended: false }));
+        this.app.use(express.static('public'));
 
-        this.route.register( this.app );
+        this.route.register(this.app);
     }
 }
 
-export default (new App()).getApp();
+export default new App().getApp();
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from 'express';
 
 export class UserController {
-    public list( req: Request, res: Response) {
-        res.status(200).json({
-            message: 'User List'
-        })
-    }
-    
-    public create( req: Request, res: Response) {
-        
-        res.status(200).json({
-            ...req.body,
-            message: 'User Create'
-        })
-    }
+  public list(req: Request, res: Response) {
+    res.status(200).json({
+      message: 'User List',
+    });
+  }
+
+  public create(req: Request, res: Response) {
+    res.status(200).json({
+      ...req.body,
+      message: 'User Create',
+    });
+  }
 }
